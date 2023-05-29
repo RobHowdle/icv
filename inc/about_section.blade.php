16631ea6dc347dfe37a5a9ea36b451303e7bdc7d
Rob Howdle <robhowdle94@gmail.com>
2023-05-29 12:04:48 +0100

    About Section Responsive Complete

--- inc/about_section.blade.php
+++ inc/about_section.blade.php
@@ -21,7 +21,55 @@
                 they may not have a clear idea themselves.
             </p>
 
-            <div class="col-wrapper">
+            <div class="col-wrapper col-wrapper--tablet">
+                <div class="left-col">
+                    <img src="assets/img/Rob.jpg" alt="Image of me from a previous job">
+                </div>
+
+                <div class="right-col">
+                    <h4 class="h5">
+                        <span class="quote">
+                            <span class="fa-solid fa-quote-left"></span>
+                            This industry can be frustrating and difficult at times, but out of all the jobs I
+                            have ever had, I have experienced no greater satisfaction than when I am presented with a
+                            challenging task, working through the problem and ultimately finding a solution that meets
+                            the clients needs and and brings them happiness especially when it would be easier to find
+                            shortcuts.
+                            <span class="fa-solid fa-quote-right"></span>
+                        </span>
+                    </h4>
+                </div>
+
+                <div class="f-width-col">
+                                        <h4 class="h6 section-sub-title">Things to know...</h4>
+                    <ul class="details-block">
+                        <li>
+                            <span class="icon fa-solid fa-chevron-right"></span>Age: <span><?php
+                            $dob = '1994-10-08';
+                            echo date_diff(date_create($dob), date_create('today'))->y;
+                            ?></span>
+                        </li>
+                        <li>
+                            <span class="icon fa-solid fa-chevron-right"></span>Years in the Industry:
+                            <span><?php echo date('Y') - '2018'; ?></span>
+                        </li>
+                        <li>
+                            <span class="icon fa-solid fa-chevron-right"></span>Town: <span>Bishop Auckland, UK</span>
+                        </li>
+                        <li>
+                            <span class="icon fa-solid fa-chevron-right"></span>Ability to drive: <span>Yes, with my own
+                                car</span>
+                        </li>
+                        <li>
+                            <span class="icon fa-solid fa-chevron-right"></span>Hobbies: <span>Building and Crafts,
+                                Music, Nature Walks, Mechanics, Photography, Late Night
+                                Drives</span>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+
+            <div class="col-wrapper col-wrapper--desktop">
                 <div class="left-col">
                     <img src="assets/img/Rob.jpg" alt="Image of me from a previous job">
                 </div>
